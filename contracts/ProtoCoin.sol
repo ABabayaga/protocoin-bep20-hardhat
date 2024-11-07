22b09722c5cea6481ae623b31a3c4deee2a3601c
ABabayaga <alef_rdl@hotmail.com>
2024-11-07 18:03:33 -0400

    primeiroupdateprotocoin

---
+++ contracts/ProtoCoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract ProtoCoin {
+    
+    string public name = "ProtoCoin";
+    string public symbol = "PRC";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 1000 * 10 ** decimals;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value); 
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value); 
+
+}
---
+++ test/ProtoCoin.test.ts
@@ -0,0 +1,40 @@
+import { loadFixture, } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("ProtoCoin Tests", function () {
+
+  async function deployFixture() {
+    const [owner, otherAccount] = await hre.ethers.getSigners();
+
+    const ProtoCoin = await hre.ethers.getContractFactory("ProtoCoin");
+    const protoCoin = await ProtoCoin.deploy();
+
+    return { protoCoin, owner, otherAccount };
+  }
+
+  it("Should have correct name", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture);
+    const name = await protoCoin.name();
+    expect(name).to.equal("ProtoCoin");
+  });
+
+  it("Should have correct symbol", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture);
+    const symbol = await protoCoin.symbol();
+    expect(symbol).to.equal("PRC");
+  });
+
+  it("Should have correct decimals", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture);
+    const decimals = await protoCoin.decimals();
+    expect(decimals).to.equal(18);
+  });
+
+  it("Should have correct totalSupply", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture);
+    const totalSupply = await protoCoin.totalSupply();
+    expect(totalSupply).to.equal(1000n * 10n **18n);
+  });
+
+});
